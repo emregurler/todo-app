c6b2f3c93f48bc3c4786cd3b4475f301c0961c75
Emre Gürler <emregurler93@gmail.com>
2021-07-03 00:07:50 +0300

    project development configurations added

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  semi: true,
+  tabWidth: 2,
+  printWidth: 100,
+  singleQuote: true,
+  trailingComma: 'all',
+  endOfLine: 'lf',
+  useTabs: false,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  jsxSingleQuote: false,
+  jsxBracketSameLine: false,
+};
