c49cc616f6e1b1426eef729feb24883c3c9a89ef
Emre Gürler <emregurler93@gmail.com>
2021-07-03 17:21:58 +0300

    edit and delete buttons added to todoitem

--- src/components/Todo/TodoItem/TodoItem.js
+++ src/components/Todo/TodoItem/TodoItem.js
@@ -2,18 +2,35 @@ import style from './TodoItem.module.scss';
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Checkbox } from 'antd';
+import { Checkbox, Button } from 'antd';
 import { getTaskStatusWithDate } from '../../../helper/dateHelper';
 
-const TodoItem = ({ id, done, title, description, deadline, onChange }) => {
+const TodoItem = ({
+  id,
+  done,
+  title,
+  description,
+  deadline,
+  onChangeCheckbox,
+  onDelete,
+  onEdit,
+}) => {
   const taskStatus = getTaskStatusWithDate(deadline, done);
 
   return (
     <div className={style.container}>
-      <Checkbox className={style.checkbox} checked={done} onChange={() => onChange(id)}>
+      <Checkbox className={style.checkbox} checked={done} onChange={() => onChangeCheckbox(id)}>
         <div className={style.title}>{title}</div>
         <div className={style.description}>{description}</div>
         <div className={`${style.deadline} ${style[`deadline__${taskStatus}`]}`}>{deadline}</div>
+        <div className={style.operationTab}>
+          <Button size="small" type="primary" danger onClick={() => onDelete(id)}>
+            Delete
+          </Button>
+          <Button size="small" onClick={() => onEdit(id)}>
+            Edit
+          </Button>
+        </div>
       </Checkbox>
     </div>
   );
@@ -25,7 +42,9 @@ TodoItem.propTypes = {
   title: PropTypes.string,
   description: PropTypes.string,
   deadline: PropTypes.string,
-  onChange: PropTypes.func,
+  onChangeCheckbox: PropTypes.func,
+  onDelete: PropTypes.func,
+  onEdit: PropTypes.func,
 };
 
 export default TodoItem;
