5af5111657ca2776ee4ae5f7b61cdc9927e09545
Emre Gürler <emregurler93@gmail.com>
2021-07-04 19:02:21 +0300

    axios baseurl change

--- src/store/services/axios.js
+++ src/store/services/axios.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const baseURL = 'http://165.227.162.75:3000/';
+const baseURL = process.env.NODE_ENV
+  ? 'http://localhost:5000'
+  : 'https://json-server-todo-app-0.herokuapp.com/';
 const api = axios.create({
   baseURL,
   headers: {
