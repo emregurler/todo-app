dd73c61fa2321d24b08175c35b5b99f783b5ba44
Emre Gürler <emregurler93@gmail.com>
2021-07-04 19:04:54 +0300

    axios production mode bugfix

--- src/store/services/axios.js
+++ src/store/services/axios.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
-const baseURL = process.env.NODE_ENV
-  ? 'http://localhost:5000'
-  : 'https://json-server-todo-app-0.herokuapp.com/';
+const baseURL =
+  process.env.NODE_ENV !== 'production'
+    ? 'http://localhost:5000'
+    : 'https://json-server-todo-app-0.herokuapp.com/';
 const api = axios.create({
   baseURL,
   headers: {
